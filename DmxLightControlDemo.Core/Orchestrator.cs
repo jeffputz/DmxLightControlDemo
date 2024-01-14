c8afde256d770669a2619bb30bff0ffcdec5409b
Jeff Putz <jeff@popw.com>
2024-01-14 15:39:36 -0500

    Working UI!

--- DmxLightControlDemo.Core/Orchestrator.cs
+++ DmxLightControlDemo.Core/Orchestrator.cs
@@ -15,8 +15,8 @@ public class Orchestrator(IDmxPollingService dmxPollingService, IStateManager st
         {
             Name = "Spot 260x - 1",
             FixtureID = 1,
-            Parameters = new List<DmxParameter>
-            {
+            Parameters =
+            [
                 new() { Channel = 29, Name = "Pan", DefaultValue = 127 },
                 new() { Channel = 30, Name = "Pan Fine", DefaultValue = 0 },
                 new() { Channel = 31, Name = "Tilt", DefaultValue = 127 },
@@ -26,12 +26,12 @@ public class Orchestrator(IDmxPollingService dmxPollingService, IStateManager st
                 new() { Channel = 35, Name = "Gobo Wheel", DefaultValue = 0 },
                 new() { Channel = 36, Name = "Gobo Rotation", DefaultValue = 0 },
                 new() { Channel = 37, Name = "Prism", DefaultValue = 0 },
-                new() { Channel = 38, Name = "Focus", DefaultValue = 0 },
+                new() { Channel = 38, Name = "Focus", DefaultValue = 127 },
                 new() { Channel = 39, Name = "Dimmer", DefaultValue = 0 },
                 new() { Channel = 40, Name = "Strobe", DefaultValue = 4 },
                 new() { Channel = 41, Name = "Function", DefaultValue = 0 },
                 new() { Channel = 42, Name = "Movement Macros", DefaultValue = 0 }
-            }
+            ]
         };
         stateManager.AddFixture(fixture1);
         
@@ -39,8 +39,8 @@ public class Orchestrator(IDmxPollingService dmxPollingService, IStateManager st
         {
             Name = "Spot 260x - 2",
             FixtureID = 2,
-            Parameters = new List<DmxParameter>
-            {
+            Parameters =
+            [
                 new() { Channel = 43, Name = "Pan", DefaultValue = 127 },
                 new() { Channel = 44, Name = "Pan Fine", DefaultValue = 0 },
                 new() { Channel = 45, Name = "Tilt", DefaultValue = 127 },
@@ -50,12 +50,12 @@ public class Orchestrator(IDmxPollingService dmxPollingService, IStateManager st
                 new() { Channel = 49, Name = "Gobo Wheel", DefaultValue = 0 },
                 new() { Channel = 50, Name = "Gobo Rotation", DefaultValue = 0 },
                 new() { Channel = 51, Name = "Prism", DefaultValue = 0 },
-                new() { Channel = 52, Name = "Focus", DefaultValue = 0 },
+                new() { Channel = 52, Name = "Focus", DefaultValue = 127 },
                 new() { Channel = 53, Name = "Dimmer", DefaultValue = 0 },
                 new() { Channel = 54, Name = "Strobe", DefaultValue = 4 },
                 new() { Channel = 55, Name = "Function", DefaultValue = 0 },
                 new() { Channel = 56, Name = "Movement Macros", DefaultValue = 0 }
-            }
+            ]
         };
         stateManager.AddFixture(fixture2);
     }
